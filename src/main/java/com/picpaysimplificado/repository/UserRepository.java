fb55eaaa35eace43a7f194280a648ac48efdd37a
Lucas Gonçalves <89371571+Lucas-G0@users.noreply.github.com>
2024-11-12 14:40:21 -0300

    feat: create user repository

---
+++ src/main/java/com/picpaysimplificado/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.repository;
+
+import com.picpaysimplificado.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByDocument(String document);
+    Optional<User> findById(Long id);
+}
